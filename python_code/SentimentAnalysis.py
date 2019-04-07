2f1320ee0643a8a86c78f19f755a9504d9b381b0
Cheryl Foo <44739672+cherylf@users.noreply.github.com>
2019-04-07 15:15:50 +0800

    adding json files containing the results for key phrases extraction

--- python_code/SentimentAnalysis.py
+++ python_code/SentimentAnalysis.py
@@ -5,8 +5,6 @@ import requests
 
 config = json.load(open("config.json"))
 text_analytics_base_url = "https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/"
-sentiment_api_url = text_analytics_base_url + "sentiment"
-print(sentiment_api_url)
 
 # read data from file which is in the same
 # directory that  your python process is based
@@ -22,11 +20,25 @@ for idx, row in df.iterrows():
         "text": row["reviewer_review"]
     })
 
+# create headers
 headers = {"Ocp-Apim-Subscription-Key": config["subscriptionKey"]}
+
+# perform sentiment analysis
+sentiment_api_url = text_analytics_base_url + "sentiment"
 response  = requests.post(sentiment_api_url, headers=headers, json=documents)
 sentiments = response.json()
 
-# save a dictionary to a json file
-outputFile = open("result.json", "w+")
-outputFile.write(json.dumps(sentiments, indent=4, sort_keys=True))
-outputFile.close()
+# save sentiments (which is a dictionary) to a json file
+outputOne = open("SentimentsResults.json", "w+")
+outputOne.write(json.dumps(sentiments, indent=4, sort_keys=True))
+outputOne.close()
+
+# perform extraction of key phrases
+key_phrases_api_url = text_analytics_base_url + "keyPhrases"
+response  = requests.post(key_phrases_api_url, headers=headers, json=documents)
+key_phrases = response.json()
+
+# save KeyTerms (which is a dictionary) to a json file
+outputTwo = open("KeyPhrasesResults.json", "w+")
+outputTwo.write(json.dumps(key_phrases, indent=4, sort_keys=True, ensure_ascii=False))
+outputTwo.close()
