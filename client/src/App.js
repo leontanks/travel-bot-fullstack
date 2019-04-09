fb8ceb47264d355f762a457eeeb45cf6dccd662f
Low, Calvin Yan Ming <calvin.yan.ming.low@merck.com>
2019-04-09 23:29:41 +0800

    Remove NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import logo from './logo.svg'
 import './App.css'
 import store from './store'
 import Customers from './components/Customer/customers'
@@ -14,12 +13,13 @@ class App extends Component {
     return (
       <Provider store={ store }>
         <div className="App">
-          <header className="App-header">
+          {/* <header className="App-header">
             <img src={ logo } className="App-logo" alt="logo"/>
             <h1 className="App-title">React/Redux Express Starter</h1>
-          </header>
+          </header> */}
           {/* <Customers/>
           <Shoppings /> */}
+          
           <MainArea />
         </div>
       </Provider>
